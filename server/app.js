6c48682b68795dd3a81bb4e7ed93a2122a84146a
KhanShaheb34 <shakirul34@student.sust.edu>
2021-09-19 00:33:22 +0600

    Handled cors error

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./src/routes/index");
 const usersRouter = require("./src/routes/userRoutes");
@@ -17,6 +18,8 @@ require("./src/database/association");
 
 const app = express();
 
+app.use(cors());
+app.options("*", cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
